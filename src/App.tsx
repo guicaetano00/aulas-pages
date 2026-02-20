3c2871a28922b6458ea7917ce2589625994f1d69
GUILHERME APARECIDO SILVA CAETANO <guilherme.caetano4@portalsesisp.org.br>
2026-02-20 16:54:43 -0300

    PAGES

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import PHome from "./pages/PHome/PHome";
+import PSobre from "./pages/PSobre/PSobre";
+import PServicos from "./pages/PServicos/PServicos";
+import PContato from "./pages/PContato/PContato";
 
 function App() {
   return (
-    <>
-    <PHome />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<PHome />} />
+        <Route path="/sobre" element={<PSobre />} />
+        <Route path="/servicos" element={<PServicos />} />
+        <Route path="/contato" element={<PContato />} />
+      </Routes>
+    </Router>
   );
 }
 
