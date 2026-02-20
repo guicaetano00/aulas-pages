f44c61cdb5c7c4d87ac0a7511299875d562bd4c3
GUILHERME APARECIDO SILVA CAETANO <guilherme.caetano4@portalsesisp.org.br>
2026-02-20 14:57:48 -0300

    PAGES

---
+++ src/pages/PContato/PContato.tsx
@@ -0,0 +1,16 @@
+import Navegacao from "../../components/Navegacao/Navegacao";
+import Sobre from "../../components/Sobre/Sobre";
+import Rodape from "../../components/Rodape/Rodape";
+
+
+function PContato () {
+  return (
+    <>
+    <Navegacao />
+    <Sobre />
+    <Rodape />
+    </>
+  )
+}
+
+export default PContato;
